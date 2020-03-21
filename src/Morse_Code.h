a618b298e9a712daef08d0521f5591b1e55bff15
Tauheed-Elahee <hobbies@tauheed-elahee.com>
2020-03-20 23:37:14 -0400

    Make header files for Morse_Code class

--- src/Morse_Code.h
+++ src/Morse_Code.h
@@ -1,6 +1,24 @@
 #ifndef MORSE_CODE_H
 #define MORSE_CODE_H
 
+#include <map>
+#include <string>
+#include "Arduino.h"
+
 // Classes might need an hpp file instead.
+
+using namespace std;
+
+class Morse_Code {
+public:
+    Morse_Code(int dotlenght = 1);
+    void charToMorse(char input);
+
+private:
+    int dotlenght;
+    static map<string, int> morse_code;
+    void dot(void);
+    void dash(void);
+};
 
 #endif//MORSE_CODE_H
